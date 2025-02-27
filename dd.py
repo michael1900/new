24ac722b59a73d266a32f34d4edcc90348f8151b
emaschi123 <test-stremio123@proton.me>
2025-02-27 09:56:25 +0100

    Update dd.py

--- dd.py
+++ dd.py
@@ -235,7 +235,7 @@ def addChannelsByLeagueSport():
 
                         UniqueID = unique_ids.pop(0)
                         try:
-                            channelName = game["event"] + " " + formatted_date_time_cet + " " + channel["channel_name"]
+                            channelName = game["event"] + " " + formatted_date_time_cet + "  " + channel["channel_name"]
                             
                             # MODIFICATO: Creare un custom tvg-id per canali del gruppo "Eventi"
                             # Estrazione della parte prima dei ":" e dopo "(CET)"
